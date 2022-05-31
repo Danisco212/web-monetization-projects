0b1568948fe8bc769334bf727e3837bd57d65bf8
Danisco212 <danielchibuikem@gmail.com>
2022-05-31 12:58:58 +0700

    chore: enable logs for dev

--- packages/coil-extension/src/util/isLoggingEnabled.ts
+++ packages/coil-extension/src/util/isLoggingEnabled.ts
@@ -1,8 +1,12 @@
 import { BuildConfig } from '../types/BuildConfig'
-import { BUILD_CONFIG } from '../webpackDefines'
+import { BUILD_CONFIG, COIL_DOMAIN } from '../webpackDefines'
 
 export function isLoggingEnabled(buildConfig: BuildConfig) {
   const enabled = Boolean(buildConfig.isLoggingEnabled)
+  const logEnabled = localStorage.getItem('COIL_LOGGING_ENABLED')
+  if (logEnabled === null && COIL_DOMAIN === 'http://localhost:4000') {
+    localStorage.COIL_LOGGING_ENABLED = true
+  }
   const override = Boolean(localStorage.COIL_LOGGING_ENABLED)
   return enabled || override
 }
